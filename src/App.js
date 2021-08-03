728b73eb1e8cdf4d6d225d5ac555ad2f46c1902d
tharon <tharonweighill@gmail.com>
2021-08-03 11:42:33 -0600

    add component folders and import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from "./components/Login";
+import About from "./components/About";
+import Map from "./components/Map";
+import News from "./components/News";
+import NewSighting from "./components/NewSighting";
+import NewUser from "./components/NewUser";
+import Sightings from "./components/Sightings";
+import Videos from "./components/Videos";
 
 function App() {
   return (
