3e78b3c1c2bb4c9f7745331e42e1137cc6eb6ff8
tharon <tharonweighill@gmail.com>
2021-08-04 10:41:08 -0600

    add new user form

---
+++ src/components/NewUserForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class NewUserForm extends Component {
+    state = {
+        name: "",
+        email: "",
+        password: ""
+    };
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default NewUserForm;
