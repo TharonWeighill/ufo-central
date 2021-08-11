22c6024252253edd984badbcfaae43918e214351
tharon <tharonweighill@gmail.com>
2021-08-11 00:02:04 -0600

    add new user compent

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createUser } from '../actions/newUser';
 
 class NewUser extends Component {
     constructor(props) {
@@ -10,11 +12,36 @@ class NewUser extends Component {
             password: '',
         };
     }
+
+    handleUsernameChange = (e) => {
+        this.setState({
+            name: e.target.value,
+        });
+    };
+
+    handleEmailChange = (e) => {
+        this.setState({
+            email: e.target.value,
+        });
+    };
+
+    handlePasswordChange = (e) => {
+        this.setState({
+            password: e.target.value,
+        });
+    };
+
     onSubmitHandler = async (e) => {
         e.preventDefault();
-        const { name, email, password } = this.state;
+        const {
+            name, email, password,
+        } = this.state;
         const { newUser, user } = this.props;
-        await newUser({ name, email, password });
+
+        await newUser({
+            name, email, password,
+        });
+
         if (user.isLogin === true) {
             const { history } = this.props;
             history.push('/');
@@ -27,45 +54,29 @@ class NewUser extends Component {
         }
     };
 
-    handleUsernameChange = (e) => { this.setState({ name: e.target.value, }); };
-
-    handleEmailChange = (e) => { this.setState({ email: e.target.value, }); };
-
-    handlePasswordChange = (e) => { this.setState({ password: e.target.value, }); };
-
     render() {
+        const { message } = this.state;
         return (
-            <form onSubmit={this.onSubmitHandler}>
+            <form onSubmit={this.onSubmitHandler} >
                 <h1>{message}</h1>
-                <input
-                    onChange={this.handleUsernameChange}
-                    type="text"
-                    name="name"
-                    placeholder="name"
-                    required
-                />
-
-                <input
-                    onChange={this.handleEmailChange}
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    required
-                />
-
-                <input
-                    onChange={this.handlePasswordChange}
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    required
-                />
-
-                <button type="submit">
-                    Create Account
-                </button>
+                <input onChange={this.handleUsernameChange} type="text" required />
+                <input onChange={this.handleEmailChange} type="email" required />
+                <input onChange={this.handlePasswordChange} type="password" required />
+                <input onChange={this.handleAvatarChange} type="file" accept="/images/*" />
+                <button type="submit">Create Account</button>
             </form>
         );
     }
 }
-export default Signup;
+const mapStateToProps = state => ({
+    user: state.user,
+});
+const mapDispatchToProps = dispatch => ({
+    newUser: estate => dispatch(createUser(estate)),
+});
+NewUser.propTypes = {
+    newUser: PropTypes.func.isRequired,
+    user: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(NewUser);
